d953c14c06b6acb46abf3ce1c6f8a2023d8009fc
Ivan <129706771+0Medsok0@users.noreply.github.com>
2024-08-14 21:53:02 +0700

    Add files via upload

---
+++ script/one.php
@@ -0,0 +1,33 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $firstname = $_POST["firstname"];
+  $phone = $_POST["phone"];
+  $email = $_POST["email"];
+  
+
+  if (empty($firstname) || empty($phone) || empty($email)) { 
+    echo "Please fill in all the fields";
+  } else {
+    $to = "smykov.iw@yandex.ru"; // емайл получателя данных из формы
+
+    $tema = "Форма  PHP"; // тема полученного емайла
+
+    $message = "firstname: " . $_POST['firstname'] . "\n";
+    $message .= "Email: " . $_POST['email'] . "\n";
+    $message .= "Phone: " . $_POST['phone'] . "\n";
+
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+    if (mail($to, $tema, $message, $headers)) {
+        echo "Ваше сообщение успешно отправлено!";
+    } else {
+        echo "При отправке сообщения произошла ошибка!";
+    }
+}
+}
+
+?>
+
